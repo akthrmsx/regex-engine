d003eacb3e3fce12f597a899172b23063e8b2c43
akthrmsx <akt.hrms@gmail.com>
2025-02-23 14:25:19 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use automaton::Regex;
+use clap::Parser;
+
+/// String matcher by regular expression
+#[derive(Debug, Parser)]
+struct Args {
+    /// Regular expression pattern
+    pattern: String,
+    /// Target text
+    text: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match Regex::new(&args.pattern) {
+        Ok(regex) => {
+            if regex.matches(&args.text) {
+                println!("{}", &args.text);
+            }
+        }
+        Err(err) => eprintln!("{}", err),
+    }
 }
